a67826d103c2d7ea8c9e559538b8915089602819
Deric Pang <dericp@cs.washington.edu>
2019-06-01 14:41:25 -0700

    NER on GPU now when tagging lambada.

--- scripts/tag_lambada.py
+++ scripts/tag_lambada.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 
+import mmap
 import argparse
 
+from tqdm import tqdm
+import torch
 import allennlp
 from allennlp.pretrained import named_entity_recognition_with_elmo_peters_2018
 
 
-PUNCT_CHARS = ['.', '!', '?']
+PUNCT_CHARS = {'.', '!', '?'}
 
 
 def main():
@@ -17,22 +20,20 @@ def main():
     args = parser.parse_args()
 
     ner_model = named_entity_recognition_with_elmo_peters_2018()
+    ner_model._model = ner_model._model.to(device=torch.device('cuda'))
     ner_model._tokenizer = allennlp.data.tokenizers.word_splitter.JustSpacesWordSplitter()
 
     with open(args.dataset_path, 'r') as lambada, open(args.output_path, 'w') as out:
-        for passage in lambada:
+        for passage in tqdm(lambada, total=get_num_lines(args.dataset_path)):
             tokens = passage.split()
-            passage_len = len(tokens)
             sentences = []
             prev_idx = -1
             for idx in range(len(tokens)):
                 token = tokens[idx]
                 # sentence segmentation
                 if idx == len(tokens)-1 or ((token in PUNCT_CHARS and tokens[idx+1] != '\'\'') or (token == '\'\'' and tokens[idx-1] in PUNCT_CHARS)):
-                    sentence = tokens[prev_idx+1:idx+1]
-                    sentences.append(' '.join(sentence))
+                    sentences.append(' '.join(tokens[prev_idx+1:idx+1]))
                     prev_idx = idx
-            assert len(tokens) == sum(len(s.split()) for s in sentences)
 
             for sentence in sentences:
                 results = ner_model.predict(sentence=sentence)
@@ -41,6 +42,15 @@ def main():
                     list_to_print.append(word + '/' + tag)
                 print(' '.join(list_to_print), file=out)
             print('', file=out)
+
+
+def get_num_lines(file_path):
+    fp = open(file_path, "r+")
+    buf = mmap.mmap(fp.fileno(), 0)
+    lines = 0
+    while buf.readline():
+        lines += 1
+    return lines
 
 
 if __name__ == '__main__':
